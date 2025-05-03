e18f40a5de7499d6ee2ee2b3c60a6692511875cd
quandang08 <22211TT1369@mail.tdc.edu.vn>
2025-05-03 22:19:37 +0700

    add Category

---
+++ src/main/java/com/amu/product/category/Category.java
@@ -0,0 +1,23 @@
+package com.amu.product.category;
+
+import com.amu.product.entities.Product;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String name;
+    private String description;
+    @OneToMany(mappedBy = "category", cascade = CascadeType.REMOVE)
+    private List<Product> products;
+}
