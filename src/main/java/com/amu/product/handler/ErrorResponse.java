92f8a60164e8c2849dbfa5988273016c16f14556
quandang08 <22211TT1369@mail.tdc.edu.vn>
2025-05-04 10:42:14 +0700

    feat: add handler

---
+++ src/main/java/com/amu/product/handler/ErrorResponse.java
@@ -0,0 +1,11 @@
+package com.amu.product.handler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ErrorResponse {
+    Map<String,String> errors;
+
+    public ErrorResponse(HashMap<String, String> errors) {
+    }
+}
